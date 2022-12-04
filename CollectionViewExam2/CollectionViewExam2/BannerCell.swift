1f932d66ba788ace621813f0784a867abd6c7216
lckdevelop <lckdevelop@gmail.com>
2022-12-04 20:07:42 +0900

    read.me

--- CollectionViewExam2/CollectionViewExam2/BannerCell.swift
+++ CollectionViewExam2/CollectionViewExam2/BannerCell.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class BannerCell: UICollectionViewCell {
+    @IBOutlet weak var BannerLabel: UILabel!    
+    @IBOutlet weak var BannerDescription: UILabel!
+    @IBOutlet weak var BannerImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 16
+    }
+    
+    func configureUI(_ info: BannerInfo) {
+        BannerLabel.text = info.title
+        BannerDescription.text = info.description
+        BannerImage.image = UIImage(named: info.imageName)
+    }
     
 }
--- CollectionViewExam2/CollectionViewExam2/PaywallViewController.swift
+++ CollectionViewExam2/CollectionViewExam2/PaywallViewController.swift
@@ -9,21 +9,69 @@ import UIKit
 
 class PaywallViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let bannerInfos: [BannerInfo] = BannerInfo.list
+    let colors:[UIColor] = [.purple, .orange, .systemPink, .systemRed]
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    enum Section {
+            case main
+        }
+    typealias Item = BannerInfo
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // presentation
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
+                return nil
+            }
+            
+            cell.configureUI(item)
+            
+            cell.backgroundColor = self.colors[indexPath.item]
+            
+            return cell
+        })
+        
+        // data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+                snapshot.appendItems(bannerInfos, toSection: .main)
+        datasource.apply(snapshot)
+        
+        //layout
+        collectionView.collectionViewLayout = updateLayout()
+        collectionView.alwaysBounceVertical = false
+                
+        self.pageControl.numberOfPages = bannerInfos.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func updateLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        // group이 cell의 크기를 벗어나자 horizontal이 아닌 vertical로 보여짐
+        // orthogonalScrollingBehavior: 수평 스크롤 사용을 명시
+        // groupPagingCentered : 페이징이 가운데를 기준으로
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = 20 // 그룹간 간격 20
+        
+        section.visibleItemsInvalidationHandler = { (items, offset, env) in
+            let index = Int((offset.x / env.container.contentSize.width).rounded(.up))
+            self.pageControl.currentPage = index
+        }
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
-    */
-
+    
 }
